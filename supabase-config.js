9ddf88e7c80f4b2b9c92f4109ccf85fae149152c
el8kun <el8kun@gmail.com>
2026-05-11 21:40:17 +0200

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -3,6 +3,6 @@
 // Copie Project URL et anon public key ici.
 
 export const supabaseConfig = {
-  url: "https://dtbqlghadlkzgakrbtib.supabase.co/rest/v1/",
+  url: "https://dtbqlghadlkzgakrbtib.supabase.co",
   anonKey: "sb_publishable_6652tF6aaYtZ1JV9V47uAw_zNlfWdly"
 };
